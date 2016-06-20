3a864c8e459ccecf025b8456134b3a9f027e1e24
Guillaume Viguier <guillaume@viguierjust.com>
2016-06-20 16:46:29 +0200

    Updated styles

--- template.php
+++ template.php
@@ -3,3 +3,7 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function pperu_preprocess_html(&$variables) {
+  drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css', array('type' => 'external'));
+}
