8518cbab5ed28ce99ce917602471fedc03691741
Guillaume Viguier <guillaume@viguierjust.com>
2016-06-20 14:45:51 +0200

    Updated styles

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -74,18 +74,20 @@
  */
 ?>
 
-<header role="banner" id="page-header">
-  <?php if (!empty($site_slogan)): ?>
-    <p class="lead"><?php print $site_slogan; ?></p>
-  <?php endif; ?>
+<div class="container">
+  <header role="banner" id="page-header">
+    <?php if (!empty($site_slogan)): ?>
+      <p class="lead"><?php print $site_slogan; ?></p>
+    <?php endif; ?>
 
-  <?php print render($page['header']); ?>
-</header> <!-- /#page-header -->
+    <?php print render($page['header']); ?>
+  </header> <!-- /#page-header -->
+</div>
 
 
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="<?php print $container_class; ?>">
-    <div class="navbar-header">
+    <div>
       <?php if ($logo): ?>
         <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
           <img class="img-responsive" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
